084fc9eca5a333e81258ec9fc833681c9af3e16d
Andrei Stoica <andreistoica12@gmail.com>
2021-02-27 20:13:38 +0200

    class Persoane added

--- sem1/src/sem1/Test.java
+++ sem1/src/sem1/Test.java
@@ -3,5 +3,9 @@ package sem1;
 public class Test {
 	public static void main(String args[]) {
 		System.out.println("Hello World!");
+		
+		Persoana p1 = new Persoana("John", "McGinn");
+		
+		System.out.println(p1.toString());
 	}
 }

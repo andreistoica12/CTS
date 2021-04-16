b798e11e60228b08fd8a7680b956ccc5f1f4589a
Andrei Stoica <andreistoica12@gmail.com>
2021-04-16 11:24:40 +0300

    Implementare Flyweight

---
+++ src/ro/ase/csie/cts/sem8recuperare/flyweight/InterfataModel3D.java
@@ -0,0 +1,8 @@
+package ro.ase.csie.cts.sem8recuperare.flyweight;
+
+public interface InterfataModel3D {
+	
+	public void incarcaModel3D(String fisier);
+	public void afisareEcran(ContextEcran context);
+	
+}

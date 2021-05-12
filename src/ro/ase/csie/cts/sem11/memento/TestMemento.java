1f0890db038ebea46eb20af8af37188a5c7171c7
Andrei Stoica <andreistoica12@gmail.com>
2021-05-12 15:26:43 +0300

    Implementare Memento

---
+++ src/ro/ase/csie/cts/sem11/memento/TestMemento.java
@@ -0,0 +1,20 @@
+package ro.ase.csie.cts.sem11.memento;
+
+public class TestMemento {
+	
+	public static void main(String[] args) {
+		
+		CaracterJoc superman = new CaracterJoc("Superman", 100, "Zboara");
+		
+		MementoCaracterJoc salvareInceputJoc = superman.salvare();
+		
+		superman.ataca();
+		superman.seVindeca(100);
+		System.out.println("Puncte viata: " + superman.puncteViata);
+		
+		superman.incarcaSalvare(salvareInceputJoc);
+		
+		System.out.println("Puncte viata: " + superman.puncteViata);
+
+	}
+}

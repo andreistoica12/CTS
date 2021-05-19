698a8717d50110d92aab362422ba66ad0b1260a6
Andrei Stoica <andreistoica12@gmail.com>
2021-05-19 14:43:13 +0300

    Implementare JUnit

---
+++ src/ro/ase/csie/cts/sem12/testare/exceptii/ExceptieImpartireLaZero.java
@@ -0,0 +1,5 @@
+package ro.ase.csie.cts.sem12.testare.exceptii;
+
+public class ExceptieImpartireLaZero extends Exception {
+	
+}
---
+++ src/ro/ase/csie/cts/sem12/testare/modele/OperatiiMatematice.java
@@ -0,0 +1,17 @@
+package ro.ase.csie.cts.sem12.testare.modele;
+
+import ro.ase.csie.cts.sem12.testare.exceptii.ExceptieImpartireLaZero;
+
+public class OperatiiMatematice {
+
+	public static int aduna(int a, int b) {
+		return a + b;
+	}
+
+	public static double impartire(int a, int b) throws ExceptieImpartireLaZero {
+		if(b == 0)
+			throw new ExceptieImpartireLaZero();
+		return a / b;
+	}
+
+}
